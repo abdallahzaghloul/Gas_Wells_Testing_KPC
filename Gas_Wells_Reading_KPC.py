ccacc56f415da05b387c422031a1c37203a3cd18
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-16 16:58:24 +0300

    Update Gas_Wells_Reading_KPC.py

--- Gas_Wells_Reading_KPC.py
+++ Gas_Wells_Reading_KPC.py
@@ -29,11 +29,12 @@ with col1:
   for i in range (1,48):
    ii=str(i)           
    try:
-    Data+"ii"=st.dataframe(conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=Well_ID+"_"+ii),width=1500,height=1500 )
+    st.dataframe(conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=Well_ID+"_"+ii),width=1500,height=1500 )
    except:
     pass            
                  
-              
+
+
             
   
   
