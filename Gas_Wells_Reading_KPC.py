6efb519e5ba9e485eb2ace06e859b5f02f9643f9
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-16 14:42:40 +0300

    Update Gas_Wells_Reading_KPC.py

--- Gas_Wells_Reading_KPC.py
+++ Gas_Wells_Reading_KPC.py
@@ -15,15 +15,11 @@ st.markdown(" <center>  <h1> Well Testing Follow UP </h1> </font> </center> </h1
 from datetime import time
 
 col1, col2, col3 = st.columns(3)
-
+worksheet="BARAKAT-D01X_16-08-2024_5"
 with col1:
  if st.button('Show Results'):
-  for i in range (1,48):           
-   try:
-    conn = st.experimental_connection("gsheets", type=GSheetsConnection)           
-    st.write(conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=Well_ID+"_"+str(i)) )            
-   except:
-    pass        
+  conn = st.experimental_connection("gsheets", type=GSheetsConnection)           
+  st.write(conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=worksheet) )            
   
             
  
