f308f33fdc736169419060a4238dc7d3f3f474aa
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-16 14:33:16 +0300

    Update and rename Gas_Wells_Reading_TMU.py to Gas_Wells_Reading_KPC.py

---
+++ Gas_Wells_Reading_KPC.py
@@ -0,0 +1,37 @@
+from PIL import Image
+import numpy as np
+import pandas as pd #2
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+
+import datetime
+im = Image.open("KPC.png")
+image= np.array(im)
+st.image(image)
+st.markdown(" <center>  <h1> Well Testing Follow UP </h1> </font> </center> </h1> ",
+            unsafe_allow_html=True)
+
+from datetime import time
+
+col1, col2, col3 = st.columns(3)
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+  
+
+with col1:
+ if st.button('Show Results'):
+  for i in range (1,48):           
+   try:
+    st.write(conn.read(worksheet=Well_ID+"_"+str(i)) )            
+   except:
+    pass        
+  
+            
+ 
+ 
+  
+
+
+ 
+
+
