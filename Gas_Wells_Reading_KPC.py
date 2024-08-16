6ec4a522825095292df0e569565ba0251c28a830
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-16 17:08:17 +0300

    Update Gas_Wells_Reading_KPC.py

--- Gas_Wells_Reading_KPC.py
+++ Gas_Wells_Reading_KPC.py
@@ -24,13 +24,14 @@ Well_ID= Well_Name+"_"+Date
 ii=""
 Data = []
 df = pd.DataFrame(Data)
+h=[]
 with col1:
  if st.button('Show Results'):
   conn = st.experimental_connection("gsheets", type=GSheetsConnection)                      
   for i in range (1,48):
    ii=str(i)           
    try:
-    h=(conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=Well_ID+"_"+ii),width=1500,height=1500 )
+    h.append(conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=Well_ID+"_"+ii),width=1500,height=1500 )
    except:
     pass            
                  
