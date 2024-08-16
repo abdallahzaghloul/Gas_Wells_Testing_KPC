36d7e5e9e16240e831dd8d9b3035b7f9d84446cd
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-16 15:37:17 +0300

    Update Gas_Wells_Reading_KPC.py

--- Gas_Wells_Reading_KPC.py
+++ Gas_Wells_Reading_KPC.py
@@ -22,15 +22,12 @@ Date=Date.strftime('%d-%m-%Y')
 
 Well_ID= Well_Name+"_"+Date
 
-
-Data=[]
-
 with col1:
  if st.button('Show Results'):
   conn = st.experimental_connection("gsheets", type=GSheetsConnection)           
   try:           
    for i in range (1,48):
-    st.write(conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=Well_ID+"_"+str(i)) )             
+    st.write(conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=Well_ID+"_"+"5") )             
   except:
    pass
               
