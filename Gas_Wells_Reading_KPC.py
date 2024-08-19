31bec20c04fdeac13752b32ccd0a1c7dbf14170d
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-19 17:30:23 +0300

    Update Gas_Wells_Reading_KPC.py

--- Gas_Wells_Reading_KPC.py
+++ Gas_Wells_Reading_KPC.py
@@ -29,13 +29,15 @@ n=int(n)
 H="R_"
 L=[]
 st.write(n)
+CK=[]
 if st.button('Show Results'):
   conn = st.experimental_connection("gsheets", type=GSheetsConnection)  
   for i in range(1,n+1):           
    L=H+str(i)
    ii=str(i)           
    L=conn.read(spreadsheet="https://docs.google.com/spreadsheets/d/1Z0clIbSazOxcYwngdQGK557s-ltIQ-Al_Ja5ypl2fgw",worksheet=Well_ID+"_"+ii,ttl="1")
-              
+   CK.append(L['C.K%'].loc[0])
+   st.write(CK)           
    st.write(L)
             
             
