9d78799f25d49a1801db160cd01f64afdd82a28d
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2024-08-19 19:12:41 +0300

    Update Gas_Wells_Reading_KPC.py

--- Gas_Wells_Reading_KPC.py
+++ Gas_Wells_Reading_KPC.py
@@ -64,7 +64,6 @@ if st.button('Show Results'):
    Water.append(L['Water'].loc[0])           
    GOR.append(L['GOR'].loc[0])           
    BSW.append(L['BS&W'].loc[0])           
-   CK=pd.DataFrame(CK)
    st.dataframe(CK)          
    st.write(Reading_No)           
    st.write(Registeration_Time)           
@@ -83,7 +82,7 @@ if st.button('Show Results'):
          
             
   
-  
+ df=Dataframe([]) 
    
  
  
